81ca3b82a1309078589f03cc788d8149573f3ea8
hp <srikanthag93@gmail.com>
2023-11-19 13:26:45 +0530

    Selenium_Automation_02

--- Data_driven_testing/Readning_data_from_Excel.py
+++ Data_driven_testing/Readning_data_from_Excel.py
@@ -1,18 +1,18 @@
 import openpyxl
 
-Path = r"C:\Users\srikanth\Desktop\IT\Testing\selenium_tutorial\Data_driven_testing\Data.xlsx"
+Path = r"C:\Users\hp\Desktop\IT\Testing\Selenium_Class\Data_driven_testing\Data.xlsx"
 
 workbook = openpyxl.load_workbook(Path)
 
 sheet = workbook.get_sheet_by_name("Sheet1")    ## if u have multiple sheets
-# sheet = workbook.active
+sheet = workbook.active
 
 
 Rows = sheet.max_row
-# print(Rows)
+print(Rows)
 
 Columns = sheet.max_column
-# print(Columns)
+print(Columns)
 
 for r in range(1,Rows+1):
     for c in range(1, Columns+1):
--- Practice.py
+++ Practice.py
@@ -1,14 +1,1 @@
-from selenium import webdriver
-from time import sleep
-from selenium.webdriver.common.keys import Keys
-
-driver = webdriver.Chrome(r"C:\Users\hp\Desktop\Chromedriver\chromedriver-win64\chromedriver.exe")
-sleep(5)
-driver.maximize_window()
-sleep(5)
-driver.get("https://demowebshop.tricentis.com/books")
-books = driver.find_elements_by_xpath("//h2[@class='product-title']")
-sleep(5)
-for book in books:
-    print(book.text)
 
