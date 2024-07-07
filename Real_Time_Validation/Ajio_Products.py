cae98c25b709893119cd328ed8bb01ce0caa099a
hp <srikanthag93@gmail.com>
2024-07-07 21:53:37 +0530

    Selenium_Automation_09

--- Real_Time_Validation/Ajio_Products.py
+++ Real_Time_Validation/Ajio_Products.py
@@ -7,25 +7,25 @@ sleep(3)
 shoe_ele = driver.find_elements_by_xpath("//div[@class='nameCls']")
 price_ele = driver.find_elements_by_xpath("//span[@class='price  ']")
 
-shoe_names = [ item.text for item in shoe_ele ]
-price_name = [ item.text for item in price_ele ]
+shoe_names = [item.text for item in shoe_ele]
+price_name = [item.text for item in price_ele]
 
 prices = []
 import re
 for price in price_name:
     match = re.findall(r'\d', price)
     prices.append(int(''.join(match)))
-#print(prices)       #to remove special char and it converts str in to int
+# print(prices)       # to remove special char, and it converts str in to int
 
 pair = []
 for shoe, price in zip(shoe_names, prices):
     pair.append({'name': shoe, 'price': price})
-#print(pair)        get price and shoe pair
+# print(pair)        get price and shoe pair
 
 max = sorted(pair, key=lambda item: item['price'])[-1]
 min = sorted(pair, key=lambda item: item['price'])[0]
 
-#range between two prices
+# range between two prices
 betwee_1000_3000 = [item for item in pair if item['price'] > 1000 and item['price'] < 3000]
 print(betwee_1000_3000)
 
