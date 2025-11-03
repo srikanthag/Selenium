22cdda3170cd38bf237618ba516a42122aa125b4
srikanthag <srikanthag93@gmail.com>
2025-11-03 21:26:15 +0530

    Selenium01

--- Practice.py
+++ Practice.py
@@ -1,13 +1,11 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 driver = webdriver.Chrome(r"C:\Users\Asus\Desktop\Test\chromedriver.exe")
-sleep(2)
 driver.get("https://www.google.com/")
-sleep(3)
-driver.find_element(By.ID, "APjFqb").send_keys("srikanth", Keys.ENTER)
-
-print(driver.title)
-driver.quit()
+sleep(2)
+driver.find_element(By.XPATH, "//textarea[@id='APjFqb']").send_keys("srikanth",Keys.ENTER)
+sleep(2)
